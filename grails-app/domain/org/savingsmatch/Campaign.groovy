d4355b5bdceefbbf9931213e6d18379c1f7762b5
Martin McGloin <martin.mcgloin@transferwise.com>
2014-10-06 21:42:56 +0300

    Added class properties for campaign

--- grails-app/domain/org/savingsmatch/Campaign.groovy
+++ grails-app/domain/org/savingsmatch/Campaign.groovy
@@ -2,6 +2,25 @@ package org.savingsmatch
 
 class Campaign {
 
+	String name
+	String description
+	String location
+	URL vimeo_url
+	Date start
+	Date end
+	BigDecimal goal
+	BigDecimal achieved
+	Boolean ended
+	Boolean success
+
     static constraints = {
+		name(blank:false)
+		description(blank: false)
+		location(blank:false)
+
     }
+
+	static mapping = {
+		description type: "text"
+	}
 }
