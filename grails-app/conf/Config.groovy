53faf61713785a11eac8d0b72e6d01cb37616a06
Martin McGloin <martin.mcgloin@transferwise.com>
2014-10-06 21:28:19 +0300

    Added Spring Security Support

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -113,3 +113,20 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'org.savingsmatch.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.savingsmatch.UserRole'
+grails.plugin.springsecurity.authority.className = 'org.savingsmatch.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/assets/**':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
